543dbdeb30159908829b452e1b31bd5e6d75daad
Rolva Mashale <mashalerolva@gmail.com>
2023-09-14 10:45:21 +0200

    updates

--- SimpliSave/Views/Controllers/Register/Register7VC.swift
+++ SimpliSave/Views/Controllers/Register/Register7VC.swift
@@ -26,7 +26,7 @@ class Register7VC: UIViewController {
         lblTarget.layer.borderWidth = 1
         lblTarget.layer.borderColor = (UIColor(red: 0.52, green: 0.03, blue: 0.34, alpha: 1.00)).cgColor
         
-        // Getting the registered userName to display - Masana - 07/08/2023
+        // Getting the registered userName to display
         
         if let user_Name = UserDefaults.standard.data(forKey: "reg1"), let name = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(user_Name) as? [String] {
             
@@ -54,18 +54,11 @@ class Register7VC: UIViewController {
                 
                 present(alertController, animated: true, completion: nil)
         }else{
-            //if the fields are not empty, the entered data is saved to userDefault 02/08/2023 Robert
+            //if the fields are not empty, the entered data is saved to userDefault
 
             let reg7 = lblTarget.text
             
             UserDefaults.standard.set(reg7, forKey: "reg7")
-            
-//            if let data = try? NSKeyedArchiver.archivedData(withRootObject: reg7, requiringSecureCoding: false) {
-//                UserDefaults.standard.set(data, forKey: "reg7")
-//                UserDefaults.standard.synchronize()
-//            }
-            
-            
             
             if let pageViewController = parent as? RegisterPageVC {
                 pageViewController.setCount = true
@@ -73,14 +66,5 @@ class Register7VC: UIViewController {
             }
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
